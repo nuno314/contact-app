04c6fabcc6ffe7450fc6d23c029beddadf6a6652
nuno314 <tinle1234vt@gmail.com>
2021-08-31 00:22:26 +0700

    update

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -4,12 +4,18 @@ class AddContact extends React.Component {
     render() {
         return (
             <div className="ui main">
-                <h2>Add Contact</h2>
+                <h1>Add Contact</h1>
                 <form className="ui form"  >
                     <div className="field">
                         <label>Name</label>
                         <input type="text" name="name" placeholder="Name"></input>
                     </div>
+            
+                    <div className="field">
+                        <label>Email</label>
+                        <input type="text" name="name" placeholder="Email"></input>
+                    </div>
+                    <button className="button">Submit</button>
                 </form>
             </div>
         )
