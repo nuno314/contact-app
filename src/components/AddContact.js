38bac01ddde4ada7b519e7a145a9520de3305b2b
nuno314 <tinle1234vt@gmail.com>
2021-09-01 23:58:06 +0700

    update

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,25 +1,52 @@
-import React from 'react';
+import React from "react";
 
 class AddContact extends React.Component {
-    render() {
-        return (
-            <div className="ui main">
-                <h1>Add Contact</h1>
-                <form className="ui form"  >
-                    <div className="field">
-                        <label>Name</label>
-                        <input type="text" name="name" placeholder="Name"></input>
-                    </div>
-            
-                    <div className="field">
-                        <label>Email</label>
-                        <input type="text" name="name" placeholder="Email"></input>
-                    </div>
-                    <button className="button">Submit</button>
-                </form>
-            </div>
-        )
+  state = {
+    name: "",
+    email: "",
+  };
+
+  add = (e) => {
+    e.preventDefault();
+    if (this.state.name === "" || this.state.email === "") {
+      alert("empty");
+      return;
     }
+    this.props.addContactHandler(this.state);
+    this.setState({ name: "", email: "" });
+  };
+
+  render() {
+    return (
+      <div className="ui main">
+        <h1>Add Contact</h1>
+        <form className="ui form" onSubmit={this.add}>
+          <div className="field">
+            <label>Name</label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={this.state.name}
+              onChange={(e) => this.setState({ name: e.target.value })}
+            ></input>
+          </div>
+
+          <div className="field">
+            <label>Email</label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Email"
+              value={this.state.email}
+              onChange={(e) => this.setState({ email: e.target.value })}
+            ></input>
+          </div>
+          <button className="button">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default AddContact;
